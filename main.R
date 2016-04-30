d127ad333b6cd62dd14d701927d66622dbaaff0b
Agata Swierc <agata.swierc@gmail.com>
2016-04-29 21:52:42 -0700

    Split demand and roofs into bands and define random sampling procedure

--- main.R
+++ main.R
@@ -44,19 +44,12 @@ date_selected_start <- "2009-05-01"
 date_selected_end <- "2009-05-31"
 
 
-demand_profiles <- dir("Data", pattern = "data.\\.csv", full.names = TRUE) %>%
-  lapply(function(path) read.table(path, header = FALSE, sep = ";")) %>%
-  do.call(what = cbind)
-save(demand_profiles, file = "Cache/demand_profiles.RData")
 
 
 #runApp(app)
 #shinyAppDir("App")
 #runApp(app, launch.browser = FALSE)
 
-roofs <- read.csv("Data/roofs.csv", sep=";")
-roofs$AzimuthRounded <- round(roofs$Azimuth / (360 / 16)) * (360 / 16)
-roofs$AngleRounded <- round(roofs$Angle / 5) * 5
 
 
 simulation_result <- run_simulation()
@@ -69,8 +62,47 @@ calculate_npv(simulation_result, 8)
 
 ggplot(roofs, aes(x = Area)) + geom_density()
 
-       
-a <- sapply(demand_profiles, sum)
+
+
+demand_profiles <- dir("Data", pattern = "data.\\.csv", full.names = TRUE) %>%
+  lapply(function(path) read.table(path, header = FALSE, sep = ";")) %>%
+  do.call(what = cbind)
+save(demand_profiles, file = "Cache/demand_profiles.RData")
+load("Cache/demand_profiles.RData")
+
+demand_profiles_sums <- sapply(demand_profiles, sum)
+demand_profiles_bands_levels <- as.numeric(cut(demand_profiles_sums,
+  quantile(demand_profiles_sums, c(0, 0.05, 0.35, 0.65 ,0.95, 1))))
+demand_profiles_bands <- list()
+demand_profiles_bands[[1]] <- which(demand_profiles_bands_levels == 2)
+demand_profiles_bands[[2]] <- which(demand_profiles_bands_levels == 3)
+demand_profiles_bands[[3]] <- which(demand_profiles_bands_levels == 4)
+
+
+roofs <- read.csv("Data/roofs.csv", sep=";")
+roofs$AzimuthRounded <- round(roofs$Azimuth / (360 / 16)) * (360 / 16)
+roofs$AngleRounded <- round(roofs$Angle / 5) * 5
+roofs$AreaBand <- as.numeric(cut(roofs$Area, c(0, quantile(roofs$Area, c(0.33, 0.66, 1)))))
+
+roofs_bands <- list()
+roofs_bands[[1]] <- which(roofs$AreaBand == 1)
+roofs_bands[[2]] <- which(roofs$AreaBand == 2)
+roofs_bands[[3]] <- which(roofs$AreaBand == 3)
+
+# Pick demand profile, roof and azimuth at random
+band <- ceiling(runif(100, max = 3))
+demand_index <- demand_profiles_bands[[band]][ceiling(runif(1, max = length(demand_profiles_bands[[band]])))]
+roof_index <- roofs_bands[[band]][ceiling(runif(1, max = length(roofs_bands[[band]])))]
+azimuth <- round(runif(1, min = 90, max = 270) / 22.5) * 22.5
+
+
+ggplot(roofs, aes(Area)) + geom_histogram()
+ggplot(roofs, aes(Azimuth)) + geom_histogram()
+ggplot(roofs, aes(Angle)) + geom_histogram()
+
+
+
+
 b <- data.frame(x = a)
 c <- b %>% filter(x < 5e4)
 ggplot(c, aes(x)) + geom_density()
